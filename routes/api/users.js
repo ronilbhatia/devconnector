148a381da28b3529e4e3696fecf838f43a302999
Ronil Bhatia <ronilbhatia@gmail.com>
2019-03-03 21:11:21 -0800

    Add validation for name length for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+// Load User model
 const User = require('../../models/User');
 const keys = require('../../config/keys');
 
@@ -18,11 +22,19 @@ router.get("/test", (req, res) => {
 // @desc   Register user
 // @access Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({
     email: req.body.email
   }).then(user => {
     if (user) {
-      return res.status(404).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(404).json({ errors });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size
@@ -85,8 +97,19 @@ router.post('/login', (req, res) => {
           } else {
             res.status(400).json({ password: 'Password incorrect - please try again' });
           }
-        })
+        });
     });
+});
+
+// @route  GET /api/users/current
+// @desc   Return current user
+// @access Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
