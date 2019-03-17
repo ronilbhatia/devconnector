ff3d92064399292ce57b88f153bb70152b5c6aba
Ronil Bhatia <ronilbhatia@gmail.com>
2019-03-16 22:12:02 -0700

    Add GET /api/posts, GET /api/posts/:id, and DELETE /api/posts/:id routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,8 @@ const passport = require("passport");
 
 // Post model
 const Post = require("../../models/Post");
+// Profile model
+const Profile = require("../../models/Profile");
 
 // Validation
 const validatePostInput = require("../../validation/post");
@@ -38,6 +40,52 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route  GET /api/posts
+// @desc   Get posts
+// @access Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noPostsFound: "No posts found" }));
+});
+
+// @route  GET /api/posts/:id
+// @desc   Get post by id
+// @access Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ noPostFound: "No post found with that id" })
+    );
+});
+
+// @route  DELETE /api/posts/:id
+// @desc   Delete post
+// @access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notAuthorized: "User not authorized" });
+          }
+
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postNotFound: "No post found" }));
+    });
   }
 );
 
